5282572a9aa0bef8dfaf81b90442934ac1ac104b
mazoku1999 <rogelio.carata10@gmail.com>
2025-01-18 17:05:16 -0400

    Primer commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  output: 'standalone',
+  // Configuraciones adicionales si las necesitas
 }
 
 module.exports = nextConfig
