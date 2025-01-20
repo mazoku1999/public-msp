eec52e384d05a4fb7abab2b2814645488aa64e3a
mazoku1999 <rogelio.carata10@gmail.com>
2025-01-20 00:20:05 -0400

    mejoramos el seo

---
+++ src/app/sitemap.ts
@@ -0,0 +1,49 @@
+import { api } from '@/services/api';
+import { MetadataRoute } from 'next';
+import { News, Video } from '@/types';
+
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+    // Obtener todos los artículos y videos
+    const articles = await api.getNews();
+    const videos = await api.getVideos();
+
+    // URLs base
+    const baseUrls: MetadataRoute.Sitemap = [
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+            lastModified: new Date(),
+            changeFrequency: 'daily' as const,
+            priority: 1,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles`,
+            lastModified: new Date(),
+            changeFrequency: 'daily' as const,
+            priority: 0.8,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/videos`,
+            lastModified: new Date(),
+            changeFrequency: 'daily' as const,
+            priority: 0.8,
+        },
+    ];
+
+    // URLs de artículos
+    const articleUrls = articles.map((article: News) => ({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/articles/${article.category}/${article.slug}`,
+        lastModified: new Date(article.created_at),
+        changeFrequency: 'weekly' as const,
+        priority: 0.6,
+    }));
+
+    // URLs de videos
+    const videoUrls = videos.map((video: Video) => ({
+        url: `${process.env.NEXT_PUBLIC_BASE_URL}/videos/${video.category}/${video.slug}`,
+        lastModified: new Date(video.created_at),
+        changeFrequency: 'weekly' as const,
+        priority: 0.6,
+    }));
+
+    return [...baseUrls, ...articleUrls, ...videoUrls];
+} 
